90510ff6a16a5283ecf2930ddb7df4a40a647d1c
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2023-01-16 19:51:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import "./Components/Home";
+import "./Components/Profile";
+import "./Components/about";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
